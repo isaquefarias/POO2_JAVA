4003154cef3b155cd6ac97fbdc67d8010c94637b
Isaque Farias <73653573+isaquefarias@users.noreply.github.com>
2022-05-04 20:59:53 -0300

    Add files via upload

---
+++ Connection-java-postgress/connectJava/connectionPostgress.java
@@ -0,0 +1,34 @@
+package connectJava;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class connectionPostgress {
+
+	public static void main(String[] args) {
+		
+		// String de Conexão
+		
+		String URL = "jdbc:postgresql://localhost:5433/postgres";
+	    String usuario = "postgres";
+	    String senha = "mysecretpassword";
+	    
+	    try {
+	    	Class.forName("org.postgresql.Driver");
+	        Connection conexao = DriverManager.getConnection(URL, usuario, senha);
+	        	System.out.println("Conexão obtida com sucesso.");
+	      }
+	      catch (SQLException ex) {
+	        System.out.println("SQLException: " + ex.getMessage());
+	        System.out.println("SQLState: " + ex.getSQLState());
+	        System.out.println("VendorError: " + ex.getErrorCode());
+	      }
+	      catch (Exception e) {
+	        System.out.println("Problemas ao tentar conectar com o banco de dados: " + e);
+	      }      
+	     
+	}
+
+}
